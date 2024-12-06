d4001b5200e98ad0033cf5907f3c5be0cbec478e
Mohammad Arze Hayat <mdarzehayat@gmail.com>
2024-12-06 20:08:58 +0530

    Update SpringBootMvcsdpProjectApplication.java

--- src/main/java/com/klef/jfsd/springboot/SpringBootMvcsdpProjectApplication.java
+++ src/main/java/com/klef/jfsd/springboot/SpringBootMvcsdpProjectApplication.java
@@ -8,7 +8,7 @@ public class SpringBootMvcsdpProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcsdpProjectApplication.class, args);
-		System.out.println("Project is Running Successfully On GitHub !!!!...");
+		System.out.println("Project is Running Successfully On GitHub !!!");
 	}
 
 }
